a6288bb055d3a7b7925142e0644332bb5fb2d819
Faithlee <lijiabin098@126.com>
2019-08-21 00:11:11 +0800

    使用JDBC的Statement执行DDL、DML语句

--- java/book/jdbc/base/Base.java
+++ java/book/jdbc/base/Base.java
@@ -15,7 +15,7 @@ public class Base {
         Class.forName("com.mysql.jdbc.Driver");
         try (
                 // 2. 使用DriverManager获取数据库连接，表示一个会话连接
-                Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.10.146:3306/test", "faithlee", "mysql@098");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "mysql", "mysql");
                 // 3. 使用Connection创建Statement对象
                 Statement statement = connection.createStatement();
                 // 4. 执行sql语句: execute()、executeQuery()、executeUpdate();
